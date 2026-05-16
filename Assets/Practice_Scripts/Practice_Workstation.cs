eda23f820ed7de0e103fca006f79f0b23f66c17e
salaar-mirza <mirzaalishaik2000@gmail.com>
2026-05-16 17:57:27 +0530

    Add Practice_Workstation script and scene object
    Introduce Practice_Workstation (implements IPracticeGrabbable) that caches MeshRenderer and changes material color on OnGrab depending on Practice_Manager state (green on ReadyToWork, red and error otherwise). Add a Workstation GameObject to Practice_Scene with Rigidbody, BoxCollider, MeshFilter and MeshRenderer components and adjust several object positions in the scene. Also update EditorUserSettings and CurrentMaximizeLayout DWLT entries (editor/layout metadata and control IDs) to reflect workspace/layout changes.

---
+++ Assets/Practice_Scripts/Practice_Workstation.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Practice_Workstation : MonoBehaviour ,IPracticeGrabbable
+{
+    // Cache the MeshRenderer component for better performance.
+    private MeshRenderer _meshRenderer;
+
+    private void Awake()
+    {
+        // Get the component once and store it in our private variable.
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void OnGrab(Transform grabPoint)
+    {
+        // Check the current state from the manager.
+        if (Practice_Manager.instance.currentState == Practice_Manager.ProcedureState.ReadyToWork)
+        {
+            Debug.Log("<color=green>SUCCESS: Workstation activated. You are following the procedure!</color>");
+            _meshRenderer.material.color = Color.green;
+        }
+        else // This 'else' block will catch AwaitingPPE and any other future states.
+        {
+            Debug.LogError("DANGER: Cannot use workstation without wearing PPE first!");
+            _meshRenderer.material.color = Color.red;
+        }
+    }
+
+    // The OnRelease method must exist to satisfy the interface, but it doesn't need to do anything for this object.
+    public void OnRelease() { }
+}
